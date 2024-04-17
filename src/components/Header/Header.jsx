e6b9b7befd095665100ba7ac2bc8b0f0617f34a4
lyvanbinh <binhboong0310@gmail.com>
2024-04-17 22:05:55 +0700

    Add lib : Thêm boxicon Thêm Tailwindcss và custom css

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,17 +1,16 @@
 import React from 'react';
-import './Header.scss';
 import { Link } from 'react-router-dom';
 import logo from './logo.jpg'
+import './Header.css'
+import 'boxicons'
 
 const Header = ({cartCount}) => {
   
   return (
-    <header className="header">
-      <div className="container">
-       
-        
-        <nav className="menu">
-          <ul>
+    <header className="header flex h-[4em] *:text-sm items-center justify-around text-[#fff] font-roboto bg-[#161516]">
+      <div className="menu flex">
+        <nav>
+          <ul className='flex'>
             <li><a href="/">Trang chủ</a></li>
             <li><Link to="/sanpham">Sản phẩm</Link></li>
             {/* <li><Link to="/region">Vùng miền</Link></li> */}
@@ -19,36 +18,21 @@ const Header = ({cartCount}) => {
             <li><Link to="/contact">Liên hệ</Link></li>
           </ul>
         </nav>
-
-        <div className="search-bar">
-          <input type="text" placeholder="Tìm kiếm..." />
-          <button type="submit">
-            <svg xmlns="http://www.w3.org/2000/svg" width="10" height="10" fill="currentColor" className="bi bi-search" viewBox="0 0 16 16">
-              <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001q.044.06.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1 1 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0" />
-            </svg>
-          </button>
-        </div>
-
-        <div className="cart-megamenu hidden-lg hidden-md">
-          <div className="cart-hover">
-            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-cart" viewBox="0 0 16 16">
-              <path d="M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .491.592l-1.5 8A.5.5 0 0 1 13 12H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5M3.102 4l1.313 7h8.17l1.313-7zM5 12a2 2 0 1 0 0 4 2 2 0 0 0 0-4m7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4m-7 1a1 1 0 1 1 0 2 1 1 0 0 1 0-2m7 0a1 1 0 1 1 0 2 1 1 0 0 1 0-2" />
-            </svg>
-            <Link to="/cart">Giỏ hàng</Link>
-            <span className="cnt count_item_pr">{cartCount}</span>
-          </div>
-        </div>
-
-        <div className="user-actions">
-          <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-person-circle" viewBox="0 0 16 16">
-            <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0" />
-            <path fillRule="evenodd" d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8m8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1" />
-          </svg>
-          <ul>
-            <li><Link to="/login">Đăng nhập</Link></li>
-            <li><Link to="/register">Đăng ký</Link></li>
-          </ul>
-        </div>
+      </div>
+      <div className="search text-[#000] flex h-[50%]">
+        <input  type="text" placeholder="Search..." class="noOutline w-[95%]" />
+        <box-icon name='search' color="white" ></box-icon>
+      </div>
+      <div className="cart cursor-pointer flex items-center">
+        Giỏ hàng
+        <box-icon name='cart' color="white"></box-icon>
+      </div>
+      <div className="account">
+        <ul className='flex'>
+          <box-icon type='solid' name='user-circle' color="white"></box-icon>
+          <li><Link to="/login">Đăng nhập /</Link></li>
+          <li><Link to="/register">Đăng ký</Link></li>
+        </ul>
       </div>
     </header>
   );
