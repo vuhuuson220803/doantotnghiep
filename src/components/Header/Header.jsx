ac4429770018313a1464b958e2301c74b8557272
lyvanbinh <binhboong0310@gmail.com>
2024-04-18 15:35:48 +0700

    Update: cập nhật lại header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,34 +3,34 @@ import { Link } from 'react-router-dom';
 import logo from './logo.jpg'
 import './Header.css'
 import 'boxicons'
+const Header = ({ cartCount }) => {
 
-const Header = ({cartCount}) => {
-  
   return (
-    <header className="header flex h-[4em] *:text-sm items-center justify-around text-[#fff] font-roboto bg-[#161516]">
-      <div className="menu flex">
+    <header className="header fixed w-full z-[100] flex h-[4em] *:text-base items-center justify-between text-[#fffff] font-roboto bg-[#ff9900]">
+      <div className="menu">
         <nav>
-          <ul className='flex'>
-            <li><a href="/">Trang chủ</a></li>
-            <li><Link to="/sanpham">Sản phẩm</Link></li>
+          <ul className='flex *:ml-4'>
+            <li className="cursor-pointer"><a href="/">Trang chủ</a></li>
+            <li className="cursor-pointer"><Link to="/sanpham">Sản phẩm</Link></li>
             {/* <li><Link to="/region">Vùng miền</Link></li> */}
-            <li><Link to="/checkout">Thanh Toán</Link></li>
-            <li><Link to="/contact">Liên hệ</Link></li>
+            <li className="cursor-pointer"><Link to="/checkout">Thanh Toán</Link></li>
+            <li className="cursor-pointer"><Link to="/contact">Liên hệ</Link></li>
           </ul>
         </nav>
       </div>
-      <div className="search text-[#000] flex h-[50%]">
-        <input  type="text" placeholder="Search..." class="noOutline w-[95%]" />
-        <box-icon name='search' color="white" ></box-icon>
+
+      <div className="search flex cursor-pointer items-center rounded-r bg-[#776262c1] w-[30%]">
+        <input type="text" placeholder="Search..." class="outline-none text-[#000] w-[95%]" />
+        <box-icon name='search' color="white" size="md"></box-icon>
       </div>
-      <div className="cart cursor-pointer flex items-center">
+      <div className="cart flex items-center">
         Giỏ hàng
-        <box-icon name='cart' color="white"></box-icon>
+        <box-icon name='cart' color="white" class="cursor-pointer" ></box-icon>
       </div>
       <div className="account">
-        <ul className='flex'>
+        <ul className='flex mr-4'>
           <box-icon type='solid' name='user-circle' color="white"></box-icon>
-          <li><Link to="/login">Đăng nhập /</Link></li>
+          <li><Link to="/login">Đăng nhập</Link></li>/
           <li><Link to="/register">Đăng ký</Link></li>
         </ul>
       </div>
